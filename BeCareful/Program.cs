d59a6a93ea960caee46cc18c9ad3160fc6e34e51
DennisVN <78357110+DennisVN@users.noreply.github.com>
2022-01-05 16:24:27 +0100

    FileInfo and Stream StreamReader and Writer

--- BeCareful/Program.cs
+++ BeCareful/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace BeCareful
 {
@@ -9,8 +10,8 @@ namespace BeCareful
         {
             try
             {
-                #region sandBoxOne
-                string rootPath = @"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful";
+                #region sandBoxGetPath
+                //string rootPath = @"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful";
 
                 //// Get directories
                 //string[] dirs = Directory.GetDirectories(rootPath,"*", SearchOption.AllDirectories);
@@ -21,8 +22,8 @@ namespace BeCareful
                 //}
 
                 //// Get Files
-                string[] files = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories);
-                string[] ExcellFiles = Directory.GetFiles(rootPath, "*.xlsx", SearchOption.AllDirectories);
+                //string[] files = Directory.GetFiles(rootPath, "*", SearchOption.AllDirectories);
+                //string[] ExcellFiles = Directory.GetFiles(rootPath, "*.xlsx", SearchOption.AllDirectories);
 
                 //foreach (string file in files)
                 //{
@@ -34,10 +35,10 @@ namespace BeCareful
                 //    Console.WriteLine($"{ (Path.GetFileName(file)) }: { info.Length } bytes");
                 //}
 
-                foreach (string Excelleke in ExcellFiles)
-                {
-                    Console.WriteLine(Excelleke);
-                }
+                //foreach (string Excelleke in ExcellFiles)
+                //{
+                //    Console.WriteLine(Excelleke);
+                //}
 
                 // // Create New Directory 
 
@@ -72,6 +73,95 @@ namespace BeCareful
                 //{
                 //    File.Move(file, $"{destinationFolder}{ Path.GetFileName(file) }"); 
                 //}
+                #endregion
+
+                #region sandBoxIO
+                DirectoryInfo myDir = new DirectoryInfo(@"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful");
+                // Read Directory info
+                Console.WriteLine(myDir.FullName);
+                Console.WriteLine(myDir.Name);
+                Console.WriteLine(myDir.Parent);
+                Console.WriteLine(myDir.Attributes);
+                Console.WriteLine(myDir.CreationTime);
+
+                DirectoryInfo dataDir = new DirectoryInfo(@"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful\UsbDrive");
+                //// Create new directory if it doesnt exists
+                //if (!Directory.Exists(dataDir.ToString()))
+                //{
+                //    Directory.CreateDirectory(dataDir.ToString());
+                //}
+                //// String to add
+                //string[] gameFiles =
+                //{
+                //    "Doom2016.exe",
+                //    "Minecraft.exe",
+                //    "GTA_V.exe"
+                //};
+                //// Create new text file path for gameFiles
+                //string textFilePath = @"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful\UsbDrive\GameFiles.txt";
+                //// Write gameFiles to the path
+                //File.WriteAllLines(textFilePath, gameFiles);
+
+                //foreach (string game in File.ReadAllLines(textFilePath))
+                //{
+                //    Console.WriteLine($"Game : {game}");
+                //}
+                // Get information from file
+
+                DirectoryInfo myDataDir = new DirectoryInfo(@"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful\UsbDrive");
+
+                FileInfo[] txtFiles = myDataDir.GetFiles("*.txt", SearchOption.AllDirectories);
+                Console.WriteLine("Matches : {0}", txtFiles.Length);
+
+                foreach(FileInfo file in txtFiles)
+                {
+                    Console.WriteLine(file.Name);
+                    Console.WriteLine(file.Length); // # bytes
+                }
+
+                // FILESTREAM : when you want to read and write byte / array of bytes
+
+                string txtFilePath2 = @"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful\UsbDrive\Soundtracks.txt";
+                // Create file
+                FileStream fs = File.Open(txtFilePath2, FileMode.Create);
+
+                string stSoundtrack = "Fireworks - Katy Perry";
+                byte[] btSoundtrackArray = Encoding.Default.GetBytes(stSoundtrack);
+
+                fs.Write(btSoundtrackArray, 0, btSoundtrackArray.Length); // 0 is index where to start
+                // change index position back to the start 
+                fs.Position = 0;
+                // Create byteArray to hold fileData
+                byte[] fileByteArray = new byte[btSoundtrackArray.Length];
+
+                // Put bytes inside Array
+                for (int i = 0; i < btSoundtrackArray.Length; i++)
+                {
+                    fileByteArray[i] = (byte)fs.ReadByte(); // cast to byte
+                }
+                // Convert byte to string for output 
+                Console.WriteLine(Encoding.Default.GetString(fileByteArray));
+                // Close Stream
+                fs.Close();
+
+                // StreamWriter and Reader when strings
+                string txtFilePath3 = @"C:\Users\BEAUCOUPDARGENT\Desktop\Be_Careful\UsbDrive\ReadMe.txt";
+                // use SW to Write text to the following path
+                StreamWriter sw = File.CreateText(txtFilePath3);
+
+                sw.WriteLine(@"¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯");
+                sw.WriteLine("README.TXT");
+                sw.WriteLine(@"¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯");
+                // Close StreamWriter
+                sw.Close();
+                // Open file for reading :
+                StreamReader sr = File.OpenText(txtFilePath3);
+
+                Console.WriteLine("Peek first char: {0}", Convert.ToChar(sr.Peek()));
+                Console.WriteLine("1st string : {0}", sr.ReadLine());
+                Console.WriteLine("Everything : {0}", sr.ReadToEnd());
+                sr.Close();
+
                 #endregion
 
                 #region stringbuilder
